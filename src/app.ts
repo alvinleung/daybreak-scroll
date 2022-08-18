7926708cc4feb09261d98f0bf89066dfa4e29e09
Alvin Leung <alvinleung2009@gmail.com>
2022-08-17 22:20:44 -0700

    debug

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,8 @@ window.addEventListener("DOMContentLoaded ", () => {
   //@ts-ignore
   const router = window.daybreak.router;
 
+  console.log("DOM content loaded, setting up scroll")
+
   function refreshScrollContainer() {
     const scrollContainer = document.querySelector(
       ".scroll-container"
