e081740dc1a86fd373c551265c10e969439b2a45
Alvin Leung <alvinleung2009@gmail.com>
2022-08-27 16:49:50 -0700

    update touch detection

--- src/scroll/scroll.ts
+++ src/scroll/scroll.ts
@@ -17,7 +17,7 @@ const isTouchDevice: any =
 export const createScroll = () => {
   const targetScroll = state(0);
   const currentScroll = state(0);
-  const useTouchInput = state(false);
+  const useTouchInput = state(isTouchDevice);
 
   const scrollContainer = state<HTMLDivElement>(document.createElement("div"));
   const scrollContent = state<HTMLDivElement>(document.createElement("div"));
@@ -75,7 +75,6 @@ export const createScroll = () => {
     // toggle scroll method
     setupScrollDOM(scrollContainer.value, scrollContent.value, useTouchInput);
   });
-  useTouchInput.set(isTouchDevice);
 
   const addScrollListeners = (newScrollContainer: HTMLDivElement) => {
     window.addEventListener("wheel", handleWheel);
